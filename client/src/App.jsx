5baffd90f092117d820f999c589c83185cc6f8e1
[JyothisShajan] <[jyothisshajan41@gmail.com]>
2022-01-22 15:28:20 +0530

    createhackathon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Auth from './pages/Auth'
-import JoinHackathon from './JoinHackathon'
-
+// import JoinHackathon from './JoinHackathon'
+import BasicTabs from './components/StepTABS/InfoTabs'
 const App = () => {
 	return (
 		<div className='App'>
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='auth' element={<Auth />} />
-				<Route path='join' element={<JoinHackathon />} />
+				{/* <Route path='join' element={<JoinHackathon />} /> */}
+				<Route path='createHackathon' element={<BasicTabs />} />
 			</Routes>
 		</div>
 	)
