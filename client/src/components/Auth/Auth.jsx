0be83fc20268d0f2bff4ad4594ca97a7c9b9608b
Alisha <alisharao0749@gmail.com>
2022-01-22 11:12:06 +0530

    added router, login page for the app

---
+++ client/src/components/Auth/Auth.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { Button, Paper, Grid, Typography, Link } from '@mui/material'
+import FeatherIcon from 'feather-icons-react'
+
+import Input from './Input'
+export default function Auth() {
+	const [showPassword, setShowPassword] = useState(false)
+	const [isSignUp, setIsSignUp] = useState(true)
+	const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword)
+
+	const handleSubmit = () => {}
+	const handleChange = () => {}
+	return (
+		<Grid container alignItems='center' justifyContent='center' sx={{ height: '100vh' }}>
+			<Grid item>
+				<Paper sx={{ p: 2 }}>
+					<form>
+						<Grid
+							container
+							direction='column'
+							justifyContent='center'
+							alignItems='center'
+							spacing={2}
+						>
+							<Grid item xs={12}>
+								<Typography variant='h5' align='center'>
+									{isSignUp ? 'Sign Up' : 'Sign In'}
+								</Typography>
+							</Grid>
+							<Grid item>
+								<FeatherIcon icon='lock' />
+							</Grid>
+							{isSignUp && (
+								<>
+									<Input name='firstName' label='First Name' handleChange={handleChange} half />
+									<Input name='firstName' label='Last Name' handleChange={handleChange} half />
+								</>
+							)}
+							<Input name='email' label='Email Address' handleChange={handleChange} type='email' />
+							<Input
+								name='password'
+								label='Password'
+								handleChange={handleChange}
+								type={showPassword ? 'text' : 'password'}
+								handleShowPassword={handleShowPassword}
+							/>
+							{isSignUp && (
+								<Input
+									name='confirmPassword'
+									label='Repeat Password'
+									handleChange={handleChange}
+									type='password'
+								/>
+							)}
+
+							<Button sx={{ mt: 2 }} type='submit' variant='contained' color='primary'>
+								{isSignUp ? 'Sign Up' : 'Sign In'}
+							</Button>
+						</Grid>
+					</form>
+				</Paper>
+				{isSignUp === true ? (
+					<Typography variant='subtitle1'>
+						Already a member? Click here to{' '}
+						<Link
+							onClick={() => {
+								setIsSignUp(!isSignUp)
+							}}
+						>
+							{' '}
+							Sign In
+						</Link>
+					</Typography>
+				) : (
+					<Typography variant='subtitle1'>
+						{' '}
+						Click{' '}
+						<Link
+							onClick={() => {
+								setIsSignUp(!isSignUp)
+							}}
+						>
+							here
+						</Link>{' '}
+						to register
+					</Typography>
+				)}
+			</Grid>
+		</Grid>
+	)
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-
+import { BrowserRouter } from 'react-router-dom'
 import { theme } from './theme'
 import App from './App'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<App />
+			<BrowserRouter>
+				<CssBaseline />
+				<App />
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
