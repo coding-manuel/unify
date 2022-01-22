6a4f811d6b8766f542bc3230eb3fbc57a37eb71e
Alisha <alisharao0749@gmail.com>
2022-01-22 16:54:39 +0530

    added dashboard, updated navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,17 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import JoinHackathon from './pages/JoinHackathon'
-
+import Dashboard from './components/Dashboard/Dashboard'
+import CreateHackathon from './pages/CreateHackathon'
 const App = () => {
 	return (
 		<div className='App'>
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='auth' element={<Auth />} />
-				{/* <Route path='join' element={<JoinHackathon />} /> */}
-				<Route path='createHackathon' element={<BasicTabs />} />
+				<Route path='join' element={<JoinHackathon />} />
+				<Route path='dash' element={<Dashboard />} />
+				<Route path='create' element={<CreateHackathon />} />
 			</Routes>
 		</div>
 	)
--- client/src/pages/CreateHackathon.jsx
+++ client/src/pages/CreateHackathon.jsx
@@ -1,114 +1,103 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepButton from '@mui/material/StepButton';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React, { useState } from 'react'
+import Box from '@mui/material/Box'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepButton from '@mui/material/StepButton'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
-
-const steps = ['Shipping address', 'Payment details', 'Review your order'];
+const steps = ['Shipping address', 'Payment details', 'Review your order']
 // const { formId, formField } = checkoutFormModel;
 
 function _renderStepContent(step) {
-  switch (step) {
-    case 0:
-      return <AddressForm formField={formField} />;
-    case 1:
-      return <PaymentForm formField={formField} />;
-    case 2:
-      return <ReviewOrder />;
-    default:
-      return <div>Not Found</div>;
-  }
+	switch (step) {
+		case 0:
+			return <AddressForm formField={formField} />
+		case 1:
+			return <PaymentForm formField={formField} />
+		case 2:
+			return <ReviewOrder />
+		default:
+			return <div>Not Found</div>
+	}
 }
 
-export default function CheckoutPage() {
-  
-  const [activeStep, setActiveStep] = useState(0);
-  const currentValidationSchema = validationSchema[activeStep];
-  const isLastStep = activeStep === steps.length - 1;
+export default function CreateHackathon() {
+	const [activeStep, setActiveStep] = useState(0)
+	const currentValidationSchema = validationSchema[activeStep]
+	const isLastStep = activeStep === steps.length - 1
 
-  function _sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+	function _sleep(ms) {
+		return new Promise((resolve) => setTimeout(resolve, ms))
+	}
 
-  async function _submitForm(values, actions) {
-    await _sleep(1000);
-    alert(JSON.stringify(values, null, 2));
-    actions.setSubmitting(false);
+	async function _submitForm(values, actions) {
+		await _sleep(1000)
+		alert(JSON.stringify(values, null, 2))
+		actions.setSubmitting(false)
 
-    setActiveStep(activeStep + 1);
-  }
+		setActiveStep(activeStep + 1)
+	}
 
-  function _handleSubmit(values, actions) {
-    if (isLastStep) {
-      _submitForm(values, actions);
-    } else {
-      setActiveStep(activeStep + 1);
-      actions.setTouched({});
-      actions.setSubmitting(false);
-    }
-  }
+	function _handleSubmit(values, actions) {
+		if (isLastStep) {
+			_submitForm(values, actions)
+		} else {
+			setActiveStep(activeStep + 1)
+			actions.setTouched({})
+			actions.setSubmitting(false)
+		}
+	}
 
-  function _handleBack() {
-    setActiveStep(activeStep - 1);
-  }
+	function _handleBack() {
+		setActiveStep(activeStep - 1)
+	}
 
-  return (
-    <React.Fragment>
-      <Typography component="h1" variant="h4" align="center">
-        Checkout
-      </Typography>
-      <Stepper activeStep={activeStep} className={classes.stepper}>
-        {steps.map(label => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <React.Fragment>
-        {activeStep === steps.length ? (
-          <CheckoutSuccess />
-        ) : (
-          <Form
-            initialValues={formInitialValues}
-            validationSchema={currentValidationSchema}
-            onSubmit={_handleSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form id={formId}>
-                {_renderStepContent(activeStep)}
+	return (
+		<React.Fragment>
+			<Typography component='h1' variant='h4' align='center'>
+				Checkout
+			</Typography>
+			<Stepper activeStep={activeStep} className={classes.stepper}>
+				{steps.map((label) => (
+					<Step key={label}>
+						<StepLabel>{label}</StepLabel>
+					</Step>
+				))}
+			</Stepper>
+			<React.Fragment>
+				{activeStep === steps.length ? (
+					<CheckoutSuccess />
+				) : (
+					<Form
+						initialValues={formInitialValues}
+						validationSchema={currentValidationSchema}
+						onSubmit={_handleSubmit}
+					>
+						{({ isSubmitting }) => (
+							<Form id={formId}>
+								{_renderStepContent(activeStep)}
 
-                <div >
-                  {activeStep !== 0 && (
-                    <Button onClick={_handleBack} >
-                      Back
-                    </Button>
-                  )}
-                  <div >
-                    <Button
-                      disabled={isSubmitting}
-                      type="submit"
-                      variant="contained"
-                      color="primary"
-                      className={classes.button}
-                    >
-                      {isLastStep ? 'Place order' : 'Next'}
-                    </Button>
-                    {isSubmitting && (
-                      <CircularProgress
-                        size={24}
-                       
-                      />
-                    )}
-                  </div>
-                </div>
-              </Form>
-            )}
-          </Form>
-        )}
-      </React.Fragment>
-    </React.Fragment>
-  );
+								<div>
+									{activeStep !== 0 && <Button onClick={_handleBack}>Back</Button>}
+									<div>
+										<Button
+											disabled={isSubmitting}
+											type='submit'
+											variant='contained'
+											color='primary'
+											className={classes.button}
+										>
+											{isLastStep ? 'Place order' : 'Next'}
+										</Button>
+										{isSubmitting && <CircularProgress size={24} />}
+									</div>
+								</div>
+							</Form>
+						)}
+					</Form>
+				)}
+			</React.Fragment>
+		</React.Fragment>
+	)
 }
